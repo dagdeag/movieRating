3a74945efda5a507deec1f6165e8adaaff01df79
Bento Leandro <bentolrosolia@gmail.com>
2022-08-07 20:05:20 -0300

    Fixed some stuff, will work next on getting the rating on rotten tomatoes.

--- movieRequests.py
+++ movieRequests.py
@@ -6,17 +6,21 @@ print("Type in the movie name or link: ")
 movie = input()
 
 movieIsLink = False
-# firstCharsLink = movie[0:3]
+firstCharsLink = movie[0:3]
 
-# if firstCharsLink == "htt" or firstCharsLink == "www":
-#     movieIsLink = True
-# else:
-#     movieIsLink = False
-# movieIsLink = movie.startswith(('http', 'www'))
-# print(movieIsLink, firstCharsLink, movie)
+if firstCharsLink == "htt" or firstCharsLink == "www":
+    movieIsLink = True
+    try:
+        movieRQ = rq.get(movie)
+    except:
+        print("Either that is not a link or the link is not working as intended.")
+else:
+    movieIsLink = False
+movieIsLink = movie.startswith(('http', 'www'))
+print(movieIsLink, firstCharsLink, movie)
 
-try:
-    movieRQ = rq.get(movie)
-except:
-    print("Either that is not a link or the link is not working as intended.")
-print("nice.")
+# try:
+#     movieRQ = rq.get(movie)
+# except:
+#     print("Either that is not a link or the link is not working as intended.")
+# print("nice.")
